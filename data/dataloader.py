2ed1713c9143d99533adcf8202ef776b2626200f
unknown <sidenerik@gmail.com>
2025-03-18 14:18:44 +0100

    changed name val_data

--- data/dataloader.py
+++ data/dataloader.py
@@ -29,7 +29,7 @@ transform = ToTensor()
 
 # Load the dataset
 tiny_imagenet_dataset_train = ImageFolder(root='/dataset/tiny-imagenet-200/train', transform=transform)
-tiny_imagenet_dataset_test = ImageFolder(root='/dataset/tiny-imagenet-200/test', transform=transform)
+tiny_imagenet_dataset_val = ImageFolder(root='/dataset/tiny-imagenet-200/test', transform=transform)
 
 # Create a dataloader
 train_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_train, batch_size=32, shuffle=True)
